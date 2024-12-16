2f57d5bc47446d84c7b41f0ed131a39c48dc8ea2
vadimborovik <v.borovuk@softum.io>
2024-12-16 15:17:17 +0200

    fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
-import {render, screen} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
-// import {act} from "react-dom/test-utils";
 
 
 // test('renders learn react link', () => {
