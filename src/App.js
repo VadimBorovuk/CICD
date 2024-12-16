4563c5f31e675f979468aebaf56c9499f6df215b
vadimborovik <v.borovuk@softum.io>
2024-12-16 12:54:09 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(prevState => prevState + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        count {count}
+        <button onClick={increment}>add</button>
+      </div>
   );
-}
+};
 
 export default App;
